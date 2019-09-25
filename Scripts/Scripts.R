4c5f1a90f514fe9ffdb4b5ed8f4ec3a903dacaca
Alexander <ale097@csiro.au>
2019-09-25 16:32:38 +1000

    Setting up a new project with BOM_data

---
+++ Scripts/Scripts.R
@@ -0,0 +1,12 @@
+.libPaths(c("C:/Users/ale097/Data School/Packages"))
+library(tidyverse)
+
+read_csv("data/BOM_data.csv")
+BOM_data <- read_csv("data/BOM_data.csv")
+
+read_csv("data/BOM_stations.csv")
+BOM_stations <- read_csv("data/BOM_stations.csv")
+
+BOM_stations
+BOM_data
+
